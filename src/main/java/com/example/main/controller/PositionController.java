e573f27565ffee5a76189d4f2cd0b5bc484c2f2c
s4rain <529895103@qq.com>
2018-12-27 09:36:24 +0800

    职位发布页面，以及发布后的成功与否判断。

--- src/main/java/com/example/main/controller/PositionController.java
+++ src/main/java/com/example/main/controller/PositionController.java
@@ -6,23 +6,28 @@ import com.example.main.model.Position;
 
 import com.example.main.service.PositionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class PositionController {
     @Autowired
 private PositionService positionService;
 @RequestMapping("/addPosition")
+@ResponseBody
 public Message AddPosition(Position position){
+    System.out.println(position.toString()+"444");
 Message message=new Message();
-if(positionService.addposition(position)!=null&&position!=null){
+if(positionService.addposition(position)!=null){
     message.setB(true);
-    message.setDes("发布职位成功");
+    message.setDes("true");
 }else {
     message.setB(false);
-    message.setDes("发布职位失败");
+    message.setDes("false");
 }
+    System.out.println(message.toString());
 return message;
 }
 
