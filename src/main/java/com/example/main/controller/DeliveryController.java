40f0e40dcb7fbff30b4f6c3549fe4bff22b7f744
838365946 <1330351736@qq.com>
2019-01-07 09:39:24 +0800

    12.28   元旦检出1.0

--- src/main/java/com/example/main/controller/DeliveryController.java
+++ src/main/java/com/example/main/controller/DeliveryController.java
@@ -1,14 +1,8 @@
 package com.example.main.controller;
 
-import com.example.main.model.Company;
-import com.example.main.model.Delivery;
 import com.example.main.service.DeliveryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 /**
  * Created by Administrator on 2019/1/4.
@@ -19,10 +13,5 @@ public class DeliveryController {
      @Autowired
      private DeliveryService deliveryService;
 
-    @RequestMapping("/resume")
-    public List<Delivery> ddd(HttpServletRequest request){
-        Company company= (Company) request.getSession().getAttribute("company");
-     return deliveryService.selectByCompany(company.getC_id());
 
-    }
 }
